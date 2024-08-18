ea20e1bf6b4b772a056ff2d14e523e77609dc4a6
Carlijavier5 <carlijavier5@gmail.com>
2024-08-18 19:01:01 -0400

    Jam Push

--- Assets/_Scripts/FurnitureManager.cs
+++ Assets/_Scripts/FurnitureManager.cs
@@ -48,12 +48,3 @@ public class FurnitureManager : MonoBehaviour {
         }
     }
 }
-
-public enum ScaleUnit { Mouse, Eagle, Frog }
-
-public class WebsiteItem : ScriptableObject {
-    public Sprite websitePNG;
-    [Range(0, 10)] public int avgReviews;
-    public string description;
-    public ScaleUnit scaleUnit;
-}
---
+++ Assets/_Scripts/WebsiteItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public enum ScaleUnit { Mouse, Eagle, Frog }
+
+public class WebsiteItem : ScriptableObject {
+    public Sprite websitePNG;
+    [Range(0, 10)] public int avgReviews;
+    public string description;
+    public ScaleUnit scaleUnit;
+}
