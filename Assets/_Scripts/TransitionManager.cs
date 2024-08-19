5cb544a4c1ede8ea224ce89d2f562563554549ef
Carlijavier5 <carlijavier5@gmail.com>
2024-08-19 00:12:21 -0400

    Jam Push

---
+++ Assets/_Scripts/TransitionManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TransitionManager : MonoBehaviour {
+
+    public static TransitionManager Instance { get; private set; }
+
+    [SerializeField] private Image image;
+    [SerializeField] private float transitionTime;
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void StartGame() {
+        AsyncOperation loadOP = SceneManager.LoadSceneAsync(1);
+        while (!loadOP.isDone) {
+
+        }
+    }
+}
